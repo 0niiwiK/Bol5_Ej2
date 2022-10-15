db8acc1dc59966b6e758210d9f9dea3f59948fa6
Cole <israsanfuen@gmail.com>
2022-10-15 14:42:10 +0200

    Ej2 Terminado

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int num=0,negativos=0,positivos=0,multiplos15=0,acumuladorpar=0;
+    for (int i = 0; i < 10; ++i) {
+        printf("Introduce un numero entero: ");
+        scanf("%d",&num);
+        if (num<0)
+            negativos++;
+        else
+            positivos++;
+        if (num%15==0)
+            multiplos15++;
+        if (num%2==0)
+            acumuladorpar=acumuladorpar+num;
+    }
+    printf("Cantidad de valores negativos: %d\n",negativos);
+    printf("Cantidad de valores positivos: %d\n",positivos);
+    printf("Cantidad de valores multiplos de 15: %d\n",multiplos15);
+    printf("Suma de valores pares: %d",acumuladorpar);
     return 0;
 }
